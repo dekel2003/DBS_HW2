80bdf3abe1b788ed8df5dd4450b2301a6137f351
Dekel Matalon <dekel2003@gmail.com>
2015-11-28 19:14:10 +0200

    add empty funcs

--- wet.c
+++ wet.c
@@ -55,3 +55,19 @@ void* addUser(const char* name)
 }
 
 
+
+void* addUserMin        (const char*    name){}
+void* removeUser        (const char*    id){}
+void* addPhoto          (const char*    user_id,
+                         const char*    photo_id){}
+void* tagPhoto          (const char*    user_id,
+                         const char*    photo_id,
+                         const char*    info);
+void* photosTags        (){}
+void* search            (const char*    word){}
+void* commonTags        (const char*    k){}
+void* mostCommonTags    (const char*    k){}
+void* similarPhotos     (const char*    k,
+                         const char*    j){}
+void* autoPhotoOnTagOn  (){}
+void* autoPhotoOnTagOFF (){}

a44b926d50e849fa075b2bd9c6d40270a069097a
Dekel Matalon <dekel2003@gmail.com>
2015-12-16 19:18:06 +0200

    .

--- wet.c
+++ wet.c
@@ -69,7 +69,7 @@ void* addUserMin        (const char*    name)
 	char cmd[2000] = {0};
 	PGresult *res;
 	
-	sprintf(cmd,"INSERT INTO users(id, name)",
+	sprintf(cmd,"INSERT INTO users(id, name) ",
 				"( select COALESCE(MIN(ID +1)) From users as t1 ",
 				"where not exists (select * from users as t2 where t1.id +1 = t2.id), '%s' )", name );
 		
